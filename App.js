556b1a6ed9e381ad9780ad2d7eab8a840ea0843a
see-simon <60693190+see-simon@users.noreply.github.com>
2021-10-28 16:39:04 +0200

    changes

--- App.js
+++ App.js
@@ -9,22 +9,25 @@ import SignIn from './components/screens/SignIn';
 import SignUp from './components/screens/SignUp';
 import ForgetPasswordSuccessFul from './components/screens/ForgetPasswordSuccessFul';
 import DashBoard from './components/screens/DashBoard';
+import paymentSuccess from './components/screens/paymentSuccess'
+
 
 
 // navigator
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 
 
 
 export default function App() {
 
-  const Stack = createStackNavigator();
-
+  
+  
+const Stack = createNativeStackNavigator();
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="SplashScreen" screenOptions={{headerShown:false}}>
+      <Stack.Navigator initialRouteName="paymentSuccess" screenOptions={{headerShown:false}}>
         <Stack.Screen name="SplashScreen" component={SplashScreen} />
         <Stack.Screen name="WelcomeScreen" component={WelcomeScreen} />
         <Stack.Screen name="SignInScreen" component={SignIn} />
@@ -32,6 +35,8 @@ export default function App() {
         <Stack.Screen name="ForgetScreen" component={ForgetPassword} />
         <Stack.Screen name="ForgetPasswordSuccessFul" component={ForgetPasswordSuccessFul} />
         <Stack.Screen name="Dashboard" component={DashBoard} />
+        
+        <Stack.Screen name="paymentSuccess" component={paymentSuccess}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
